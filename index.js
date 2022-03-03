1251278ec2841231b3c7d06a7cff34707494784e
odettelittle <Odettelittle1@gmail.com>
2022-03-03 20:36:42 +0000

    commituno

---
+++ index.js
@@ -0,0 +1,34 @@
+import 'bootstrap';
+
+import '../style/index.scss';
+
+window.onload = () => {
+  parenthesis
+  let randomNumber [] = Math.random() *10;
+  document.querySelector ()
+}
+
+let generateExcuse  = () => {
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+
+let whoIndex = Math.floor(Math.random() * who.legth);
+let actionIndex =Math.floor(Math.random() * action.legth);
+let whatIndex =Math.floor(Math.random() * what.legth);
+let whenIndex =Math.floor(Math.random() * when.legth);
+
+return who[whoIndex] + '' + action[actionIndex] + '' + what[whatIndex] + '' + when[whenIndex];
+};
+
+
+
